3868a9322017d4af0ecb40d35722f2e611d9f283
sinthier <135516151+sinthier@users.noreply.github.com>
2025-03-18 01:04:57 +0630

    Delete newsapp/presentation directory

--- newsapp/presentation/home/HomeState.kt
+++
@@ -1,7 +0,0 @@
-package com.loc.newsapp.presentation.home
-
-data class HomeState(
-    val scrollValue: Int = 0,
-    val maxScrollingValue: Int = 0,
-    val bookmarkedArticles: Set<String> = emptySet()
-)
